be84ad285a49b487ea672cfc24257fe398e2c0bb
xtity <kousuke.sima@gmail.com>
2015-04-17 00:25:16 +0900

    Modify dialyzer.plt 7

--- lib/mix/tasks/dialyzer.plt.ex
+++ lib/mix/tasks/dialyzer.plt.ex
@@ -131,9 +131,10 @@ defmodule Mix.Tasks.Dialyzer.Plt do
 
   defp core_plt_contains?(app, plt_file) do
     app = to_char_list(app)
-    puts app
+    puts "app: " <> app
     plt_file = to_char_list(plt_file)
-    puts plt_file
+    puts "plt_file: " <> plt_file
+    puts "plt_info: " <> :dialyzer.plt_info(plt_file)
     :dialyzer.plt_info(plt_file)
     |> elem(1) |> Keyword.get(:files)
     |> Enum.find(fn(s) ->
